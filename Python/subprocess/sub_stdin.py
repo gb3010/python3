6d9043248a31662f6ca0cc9ddefc7244b8eee8c4
Ganesh Babu <readingroomofgb@gmail.com>
2021-08-27 18:52:52 +0530

    added few more files

---
+++ Python/subprocess/sub_stdin.py
@@ -0,0 +1,13 @@
+from subprocess import Popen,PIPE
+import random
+l = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+p = Popen(['sort'],stdin=PIPE,stdout=PIPE)
+
+for i in range(5):
+    p.stdin.write(random.choice(l).encode())
+
+p.stdin.close()
+
+for j in p.stdout:
+    print(j.decode(),end='')
