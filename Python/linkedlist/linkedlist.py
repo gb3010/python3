a80fcb2fa921d32d3023553f6ba9f53eb0461bdf
Ganesh Babu <readingroomofgb@gmail.com>
2022-03-11 11:12:54 +0530

    Added insert at position function

--- Python/linkedlist/linkedlist.py
+++ Python/linkedlist/linkedlist.py
@@ -9,9 +9,10 @@ class Llist:
 		self.head = None 
 	
 	def printnodes(self):
-		while self.head is not None:
-			print(self.head.data)
-			self.head = self.head.next 
+		head_pointer = self.head
+		while head_pointer is not None:
+			print(head_pointer.data)
+			head_pointer = head_pointer.next
 
 	def insertatbeginning(self,data):
 		new_node = Node(data)
@@ -20,15 +21,32 @@ class Llist:
 
 	def insertatend(self,data):
 		last_node = Node(data)
+		head_ptr_ins = self.head
 		if self.head == None:
 			self.head = last_node
 		else:
-			while self.head:
-				traverse_node = self.head
-				self.head = self.head.next 
+			while head_ptr_ins:
+				traverse_node = head_ptr_ins
+				head_ptr_ins = head_ptr_ins.next
 			
 			traverse_node.next = last_node
 
+	def insertatposition(self,data,position):
+		new_node_insert = Node(data)
+		counter = 1
+		pointer = self.head
+
+
+		while pointer.next is not None:
+			
+
+			if counter == position:
+				new_node_insert.next = pointer.next
+				pointer.next = new_node_insert
+				break
+		
+			counter += 1
+			pointer = pointer.next
 
 
 
@@ -43,12 +61,17 @@ n1.next = n2
 n2.next = n3 
 
 
-#l1.printnodes()
-
+l1.printnodes()
+print()
 l1.insertatbeginning(0)
 
 l1.printnodes()
 
 l1.insertatend(4)
+print()
+l1.printnodes()
 
+
+l1.insertatposition(44,2)
+print()
 l1.printnodes()
